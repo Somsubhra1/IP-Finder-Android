aea68f4e346ecffc1b79718fd43f4f6a688d8a87
Somsubhra <das.somsubhra1@gmail.com>
2018-01-18 23:24:29 +0530

    Changed layout of app and added webview to open URL

--- app/src/main/java/com/somsubhra/ipfinder/MainActivity.java
+++ app/src/main/java/com/somsubhra/ipfinder/MainActivity.java
@@ -1,17 +1,20 @@
 package com.somsubhra.ipfinder;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText urlText;
     private Button button;
-    private TextView hostText, ipText;
+    private TextView hostText, ipText, host;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +25,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         urlText = findViewById(R.id.urlText);
         button = findViewById(R.id.button);
+        host = findViewById(R.id.host);
         hostText = findViewById(R.id.hostText);
         ipText = findViewById(R.id.ipText);
+
+        Toast.makeText(this, "Created by SOMSUBHRA DAS", Toast.LENGTH_LONG).show();
 
         button.setOnClickListener(this);  //adding listener to button
     }
     @Override
     public void onClick(View view) {
 
-        String url = urlText.getText().toString(); // fetch URL
+        final String url = urlText.getText().toString(); // fetch URL
 
         //checks if URL field is empty:
 
         if( url.equals("") ) {
+            host.setText("");
+            hostText.setText("");
             ipText.setText(R.string.url_null);
             return;
         }
@@ -48,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //Checks for error:
 
             if ( hostIP.equals("E") ) {
+                host.setText("");
                 hostText.setText("");
                 ipText.setText(R.string.error_msg);
                 return;
@@ -55,11 +64,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             //Updating components:
 
-            hostText.setText(new StringBuilder().append("Host URL: ").append(url).toString());
+            Toast.makeText(this, "IP Found Successfully", Toast.LENGTH_SHORT).show();
+            host.setText(R.string.host);
+            hostText.setPaintFlags(hostText.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            hostText.setText(url);
             ipText.setText(new StringBuilder().append("Host IP: ").append(hostIP).toString());
 
-         } catch (Exception e) {
+            // Setting hyperlink to open the URL in WebView:
 
+            hostText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "Opening "+urlText.getText().toString(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Openweb.class); // WebView Intent
+                    intent.putExtra("URL", urlText.getText().toString());
+                    startActivity(intent);
+                }
+            });
+
+         } catch (Exception e) {
+            host.setText("");
             hostText.setText("");
             ipText.setText(R.string.error_msg);
 
