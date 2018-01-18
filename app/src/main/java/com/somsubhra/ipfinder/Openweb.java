440056badea52979ea51e0510255a0860fa3c1a2
Somsubhra <das.somsubhra1@gmail.com>
2018-01-18 21:16:25 +0530

    Added WebView to open URL

---
+++ app/src/main/java/com/somsubhra/ipfinder/Openweb.java
@@ -0,0 +1,34 @@
+package com.somsubhra.ipfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class Openweb extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_openweb);
+
+        //Linking components:
+
+        webView = findViewById(R.id.webView);
+
+        //Opening URL:
+
+        String url = getIntent().getStringExtra("URL");
+        url = "https://" + url;
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+    }
+}
